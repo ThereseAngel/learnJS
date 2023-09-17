84fa6d2a137f24e089cf09843a0bdb2f59d15a21
Therese Angel Saavedra <72799911+ThereseAngel@users.noreply.github.com>
2023-09-17 13:38:04 +0000

    Assignment Operators

---
+++ assignmentOperators.js
@@ -0,0 +1,24 @@
+// Assignment Operators
+
+// Variables
+let num1 = 10;
+let num2 = num1 + 5;
+console.log(num2);
+
+// Addition
+num1 = num1 + 1;
+num1 += 1;
+
+// Subtraction
+num1 = num1 - 5;
+num1 -= 5;
+
+// Multiplication
+num1 = num1 * 5;
+num1 *= 5;
+
+// Increment
+num1++;
+
+// Decrement
+num1--;
