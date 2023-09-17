a6637a7c1d778d3270251e758bd27c0019d2ec6a
Therese Angel Saavedra <72799911+ThereseAngel@users.noreply.github.com>
2023-09-17 13:18:14 +0000

    Arithmetic Operators JS

---
+++ arithmeticOperators.js
@@ -0,0 +1,23 @@
+// Arithmetic Operators
+
+//Variables
+let num1 = 10;
+let num2 = 5;
+
+//Addition
+console.log(num1 + num2);
+
+//Subtraction
+console.log(num1 - num2);
+
+//Multiplication
+console.log(num1 * num2);
+
+//Division
+console.log(num1 / num2);
+
+//Squared
+console.log(num1 ** num2);
+
+//Modulo
+console.log(num1 % num2);
